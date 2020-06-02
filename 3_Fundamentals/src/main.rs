47f4b1f1965f40fbe6617c4c240b99ba7071b542
Robson Ramos <rosbonramosabrao@hotmail.com>
2020-06-02 10:11:33 -0300

    programming fundamentals

--- 3_Fundamentals/src/main.rs
+++ 3_Fundamentals/src/main.rs
@@ -1,3 +1,42 @@
+const MAX_POINTS: u32 = 100_000;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let y = 5;
+
+    let y = y + 1;
+
+    let y = y * 2;
+
+     println!("The value of x is: {}", y);
+
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+    
+    
+    let x = 2.0; // f64
+
+    let y: f32 = 3.0; // f32
+
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+
+    // remainder
+    let remainder = 43 % 5;
 }
---
+++ 3_Fundamentals/target/debug/deps/hello_cargo-e3836fecfdbdcb06.d
@@ -0,0 +1,5 @@
+/Users/RobsonRamos/Documents/GitHub/RustLang/3_Fundamentals/target/debug/deps/hello_cargo-e3836fecfdbdcb06: src/main.rs
+
+/Users/RobsonRamos/Documents/GitHub/RustLang/3_Fundamentals/target/debug/deps/hello_cargo-e3836fecfdbdcb06.d: src/main.rs
+
+src/main.rs:
