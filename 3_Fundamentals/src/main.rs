dc3e4b0fe0b05a8cfefc8f73dac1a1054d1bbd72
Robson Ramos <rosbonramosabrao@hotmail.com>
2020-06-03 20:56:54 -0300

    rust fundamentals

--- 3_Fundamentals/src/main.rs
+++ 3_Fundamentals/src/main.rs
@@ -39,4 +39,39 @@ fn main() {
 
     // remainder
     let remainder = 43 % 5;
+
+    // booleans
+    let t = true;
+    let f : bool = false;
+
+    // characters
+    let c = 'z';
+    let heart_eyed_cat = '😻'; // emoji
+
+    //
+    // compound types
+    //
+
+    // Tuples => Tuples have a fixed length: once declared, they cannot grow or shrink in size.
+    let tup : (i32, f64, u8) = (500, 6.4, 1); 
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {}", y);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    // Arrays => Unlike a tuple, every element of an array must have the same type. Arrays in Rust are different from arrays in some other languages because arrays in Rust have a fixed length
+    let a = [1, 2, 3, 4, 5];
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5]; 
+
+    let first = a[0];
+    let second = a[1];
 }
