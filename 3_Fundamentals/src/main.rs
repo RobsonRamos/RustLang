b00b1559ce1e34b8f9ca48d3537d86dc7423bae4
Robson Ramos <rosbonramosabrao@hotmail.com>
2020-06-03 21:39:07 -0300

    fundamentals

--- 3_Fundamentals/src/main.rs
+++ 3_Fundamentals/src/main.rs
@@ -106,3 +106,49 @@ fn five() -> i32{
 fn plus_one(x: i32) -> i32 {
     x + 1
 }
+
+fn function_with_condition(){
+
+    let condition = true;
+
+    let number = if condition { 5 } else { 6};
+
+    println!("The value of number is: {}", number);
+
+}
+
+fn function_with_loops(){
+
+    loop {
+        // println!("again!");
+    }
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+}
